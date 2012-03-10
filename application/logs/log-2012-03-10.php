3f4bd2657563cd3f90ea10134fd7d2f458f99706
Wessley McInroy <cheeseweasel@gmail.com>
2012-03-10 12:38:19 +0000

    added sparks#

--- application/logs/log-2012-03-10.php
+++ application/logs/log-2012-03-10.php
@@ -14,3 +14,74 @@ DEBUG - 2012-03-10 09:36:08 --> Language Class Initialized
 DEBUG - 2012-03-10 09:36:08 --> Loader Class Initialized
 DEBUG - 2012-03-10 09:36:08 --> Controller Class Initialized
 ERROR - 2012-03-10 09:36:08 --> 404 Page Not Found --> test999
+DEBUG - 2012-03-10 09:44:27 --> Config Class Initialized
+DEBUG - 2012-03-10 09:44:27 --> Hooks Class Initialized
+DEBUG - 2012-03-10 09:44:27 --> Utf8 Class Initialized
+DEBUG - 2012-03-10 09:44:27 --> UTF-8 Support Enabled
+DEBUG - 2012-03-10 09:44:27 --> URI Class Initialized
+DEBUG - 2012-03-10 09:44:27 --> Router Class Initialized
+DEBUG - 2012-03-10 09:44:27 --> Output Class Initialized
+DEBUG - 2012-03-10 09:44:27 --> Security Class Initialized
+DEBUG - 2012-03-10 09:44:27 --> Input Class Initialized
+DEBUG - 2012-03-10 09:44:27 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-03-10 09:44:27 --> Language Class Initialized
+DEBUG - 2012-03-10 09:44:27 --> Loader Class Initialized
+DEBUG - 2012-03-10 09:44:27 --> Controller Class Initialized
+ERROR - 2012-03-10 09:44:27 --> 404 Page Not Found --> test999
+DEBUG - 2012-03-10 09:49:45 --> Config Class Initialized
+DEBUG - 2012-03-10 09:49:45 --> Hooks Class Initialized
+DEBUG - 2012-03-10 09:49:45 --> Utf8 Class Initialized
+DEBUG - 2012-03-10 09:49:45 --> UTF-8 Support Enabled
+DEBUG - 2012-03-10 09:49:45 --> URI Class Initialized
+DEBUG - 2012-03-10 09:49:45 --> Router Class Initialized
+DEBUG - 2012-03-10 09:49:45 --> Output Class Initialized
+DEBUG - 2012-03-10 09:49:45 --> Security Class Initialized
+DEBUG - 2012-03-10 09:49:45 --> Input Class Initialized
+DEBUG - 2012-03-10 09:49:45 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-03-10 09:49:45 --> Language Class Initialized
+DEBUG - 2012-03-10 09:49:45 --> Loader Class Initialized
+DEBUG - 2012-03-10 09:49:45 --> Controller Class Initialized
+ERROR - 2012-03-10 09:49:45 --> 404 Page Not Found --> test999
+DEBUG - 2012-03-10 09:49:46 --> Config Class Initialized
+DEBUG - 2012-03-10 09:49:46 --> Hooks Class Initialized
+DEBUG - 2012-03-10 09:49:46 --> Utf8 Class Initialized
+DEBUG - 2012-03-10 09:49:46 --> UTF-8 Support Enabled
+DEBUG - 2012-03-10 09:49:46 --> URI Class Initialized
+DEBUG - 2012-03-10 09:49:46 --> Router Class Initialized
+DEBUG - 2012-03-10 09:49:46 --> Output Class Initialized
+DEBUG - 2012-03-10 09:49:46 --> Security Class Initialized
+DEBUG - 2012-03-10 09:49:46 --> Input Class Initialized
+DEBUG - 2012-03-10 09:49:46 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-03-10 09:49:46 --> Language Class Initialized
+DEBUG - 2012-03-10 09:49:46 --> Loader Class Initialized
+DEBUG - 2012-03-10 09:49:46 --> Controller Class Initialized
+ERROR - 2012-03-10 09:49:46 --> 404 Page Not Found --> test999
+DEBUG - 2012-03-10 09:50:04 --> Config Class Initialized
+DEBUG - 2012-03-10 09:50:04 --> Hooks Class Initialized
+DEBUG - 2012-03-10 09:50:04 --> Utf8 Class Initialized
+DEBUG - 2012-03-10 09:50:04 --> UTF-8 Support Enabled
+DEBUG - 2012-03-10 09:50:04 --> URI Class Initialized
+DEBUG - 2012-03-10 09:50:04 --> Router Class Initialized
+DEBUG - 2012-03-10 09:50:04 --> Output Class Initialized
+DEBUG - 2012-03-10 09:50:04 --> Security Class Initialized
+DEBUG - 2012-03-10 09:50:04 --> Input Class Initialized
+DEBUG - 2012-03-10 09:50:04 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-03-10 09:50:04 --> Language Class Initialized
+DEBUG - 2012-03-10 09:50:04 --> Loader Class Initialized
+DEBUG - 2012-03-10 09:50:04 --> Controller Class Initialized
+ERROR - 2012-03-10 09:50:04 --> 404 Page Not Found --> test9999
+DEBUG - 2012-03-10 09:55:23 --> Config Class Initialized
+DEBUG - 2012-03-10 09:55:23 --> Hooks Class Initialized
+DEBUG - 2012-03-10 09:55:23 --> Utf8 Class Initialized
+DEBUG - 2012-03-10 09:55:23 --> UTF-8 Support Enabled
+DEBUG - 2012-03-10 09:55:23 --> URI Class Initialized
+DEBUG - 2012-03-10 09:55:23 --> Router Class Initialized
+DEBUG - 2012-03-10 09:55:23 --> No URI present. Default controller set.
+DEBUG - 2012-03-10 09:55:23 --> Output Class Initialized
+DEBUG - 2012-03-10 09:55:23 --> Security Class Initialized
+DEBUG - 2012-03-10 09:55:23 --> Input Class Initialized
+DEBUG - 2012-03-10 09:55:23 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-03-10 09:55:23 --> Language Class Initialized
+DEBUG - 2012-03-10 09:55:23 --> Loader Class Initialized
+DEBUG - 2012-03-10 09:55:23 --> Controller Class Initialized
+ERROR - 2012-03-10 09:55:23 --> 404 Page Not Found --> 
